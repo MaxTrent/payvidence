e9a9cba4b13d15055070f576247956762d8f2cdc
Samuel <samueladelowo92@gmail.com>
2024-12-06 01:59:21 +0100

    homepage transactions

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -8,6 +8,7 @@ const Color appGrey = Color(0xffD9D9D9);
 const Color appGrey1 = Color(0xffF8F8F8);
 const Color appGrey2 = Color(0xffE8EBEE);
 const Color appGrey3 = Color(0xffEAE7F6);
+const Color appGrey4 = Color(0xff979797);
 const Color borderGrey = Color(0xffE9E9E9)
 ;const Color scaffoldBackground = Color(0xffE3DDFF);
 const Color borderColor = Color(0xffB8B8B8);
