98b683e4b311b156b617f0c63c3552c71b8c94ee
habyaad <adeomotoshan@gmail.com>
2025-03-02 13:46:24 +0100

    feat: implement routing to login when token expires

--- lib/data/network/interceptors/connection_interceptor.dart
+++ lib/data/network/interceptors/connection_interceptor.dart
@@ -1,5 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:payvidence/routes/payvidence_app_router.dart';
+import 'package:payvidence/shared_dependency/shared_dependency.dart';
+import 'package:payvidence/utilities/toast_service.dart';
 
 class ConnectionStatusInterceptor extends InterceptorsWrapper {
   @override
@@ -17,11 +20,19 @@ class ConnectionStatusInterceptor extends InterceptorsWrapper {
   @override
   void onResponse(
       Response<dynamic> response, ResponseInterceptorHandler handler) {
+
     handler.next(response);
   }
 
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) {
+
+    if (err.response?.statusCode == 401) {
+      // Redirect to the login page
+      locator<PayvidenceAppRouter>().navigateNamed(PayvidenceRoutes.login);
+      //ToastService.error(locator<PayvidenceAppRouter>().navigatorKey.currentContext, "Session expired, login again!");
+      return;
+    }
     handler.next(err);
   }
 }
