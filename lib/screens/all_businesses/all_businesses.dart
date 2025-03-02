d65130d9e106aa1e32f90d809ae0414f8ca97fe7
habyaad <adeomotoshan@gmail.com>
2025-03-02 13:43:43 +0100

    feat: implement fetch all business remote data in screen

--- lib/screens/all_businesses/all_businesses.dart
+++ lib/screens/all_businesses/all_businesses.dart
@@ -1,47 +1,68 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:payvidence/constants/app_colors.dart';
 
 import '../../components/business_card.dart';
-
+import '../../model/business_model.dart';
 
 @RoutePage(name: 'AllBusinessesRoute')
-class AllBusinesses extends StatelessWidget {
-  const AllBusinesses({super.key});
+class AllBusinesses extends ConsumerWidget {
+  final List<Business> allBusiness;
+  const AllBusinesses(this.allBusiness, {super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
-        padding:  EdgeInsets.symmetric(horizontal: 20.w),
-        child: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 20.w),
+        child: Column(
           children: [
-            SizedBox(height: 16.h,),
+            SizedBox(
+              height: 16.h,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('All businesses', style: Theme.of(context).textTheme.displayLarge,),
-              Text('+ Add New', style: Theme.of(context).textTheme.displayMedium!.copyWith(fontSize: 14.sp, color: primaryColor2))
+                Text(
+                  'All businesses',
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+                GestureDetector(
+                  onTap: (){
+
+                  },
+                  child: Text('+ Add New',
+                      style: Theme.of(context)
+                          .textTheme
+                          .displayMedium!
+                          .copyWith(fontSize: 14.sp, color: primaryColor2)),
+                )
               ],
             ),
-            SizedBox(height: 32.h,),
-            BusinessCard(
-              businessName: 'Keekee Store',
-              numberOfReceipts: '304',
-              numberOfInvoices: '214',
+            SizedBox(
+              height: 32.h,
             ),
-            SizedBox(height: 24.h,),
-            BusinessCard(
-              businessName: 'Keekee Electronics',
-              numberOfReceipts: '94',
-              numberOfInvoices: '51',
-            ),
+            ListView.separated(
+              shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return BusinessCard(
+                    business: allBusiness[index],
+
+                  );
+                },
+                separatorBuilder: (ctx, idx) {
+                  return SizedBox(
+                    height: 24.h,
+                  );
+                },
+                itemCount: allBusiness.length),
           ],
         ),
       ),
     );
   }
 }
-
