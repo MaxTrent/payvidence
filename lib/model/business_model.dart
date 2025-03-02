a24ffad0e2f8e213b3cdff19821b952d8b2ad1d9
habyaad <adeomotoshan@gmail.com>
2025-03-02 13:50:04 +0100

    feat: extend business model with equatable

--- lib/model/business_model.dart
+++ lib/model/business_model.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
-class Business {
+import 'package:equatable/equatable.dart';
+
+class Business extends Equatable {
   final String? id;
   final String? accountId;
   final String? name;
@@ -17,7 +19,7 @@ class Business {
   final DateTime? createdAt;
   final DateTime? updatedAt;
 
-  Business({
+  const Business({
     this.id,
     this.accountId,
     this.name,
@@ -35,43 +37,67 @@ class Business {
     this.updatedAt,
   });
 
-  factory Business.fromRawJson(String str) => Business.fromJson(json.decode(str));
+  @override
+  List<Object?> get props => [
+        id,
+        accountId,
+        name,
+        address,
+        phoneNumber,
+        logoUrl,
+        issuer,
+        issuerRole,
+        issuerSignatureUrl,
+        bankName,
+        accountNumber,
+        accountName,
+        vat,
+        createdAt,
+        updatedAt,
+      ];
+
+  factory Business.fromRawJson(String str) =>
+      Business.fromJson(json.decode(str));
 
   String toRawJson() => json.encode(toJson());
 
   factory Business.fromJson(Map<String, dynamic> json) => Business(
-    id: json["id"],
-    accountId: json["account_id"],
-    name: json["name"],
-    address: json["address"],
-    phoneNumber: json["phone_number"],
-    logoUrl: json["logo_url"],
-    issuer: json["issuer"],
-    issuerRole: json["issuer_role"],
-    issuerSignatureUrl: json["issuer_signature_url"],
-    bankName: json["bank_name"],
-    accountNumber: json["account_number"],
-    accountName: json["account_name"],
-    vat: json["vat"],
-    createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
-    updatedAt: json["updated_at"] == null ? null : DateTime.parse(json["updated_at"]),
-  );
+        id: json["id"],
+        accountId: json["account_id"],
+        name: json["name"],
+        address: json["address"],
+        phoneNumber: json["phone_number"],
+        logoUrl: json["logo_url"],
+        issuer: json["issuer"],
+        issuerRole: json["issuer_role"],
+        issuerSignatureUrl: json["issuer_signature_url"],
+        bankName: json["bank_name"],
+        accountNumber: json["account_number"],
+        accountName: json["account_name"],
+        vat: json["vat"],
+        createdAt: json["created_at"] == null
+            ? null
+            : DateTime.parse(json["created_at"]),
+        updatedAt: json["updated_at"] == null
+            ? null
+            : DateTime.parse(json["updated_at"]),
+      );
 
   Map<String, dynamic> toJson() => {
-    "id": id,
-    "account_id": accountId,
-    "name": name,
-    "address": address,
-    "phone_number": phoneNumber,
-    "logo_url": logoUrl,
-    "issuer": issuer,
-    "issuer_role": issuerRole,
-    "issuer_signature_url": issuerSignatureUrl,
-    "bank_name": bankName,
-    "account_number": accountNumber,
-    "account_name": accountName,
-    "vat": vat,
-    "created_at": createdAt?.toIso8601String(),
-    "updated_at": updatedAt?.toIso8601String(),
-  };
+        "id": id,
+        "account_id": accountId,
+        "name": name,
+        "address": address,
+        "phone_number": phoneNumber,
+        "logo_url": logoUrl,
+        "issuer": issuer,
+        "issuer_role": issuerRole,
+        "issuer_signature_url": issuerSignatureUrl,
+        "bank_name": bankName,
+        "account_number": accountNumber,
+        "account_name": accountName,
+        "vat": vat,
+        "created_at": createdAt?.toIso8601String(),
+        "updated_at": updatedAt?.toIso8601String(),
+      };
 }
