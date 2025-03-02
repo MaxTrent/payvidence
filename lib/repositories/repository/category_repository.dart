d8d39957b222c3437ce0908668e4220edc447d01
habyaad <adeomotoshan@gmail.com>
2025-03-02 13:42:32 +0100

    feat: create category repository

---
+++ lib/repositories/repository/category_repository.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:payvidence/model/category_model.dart';
+
+import '../../datasource/data/category_datasource.dart';
+
+abstract class ICategoryRepository {
+  Future<CategoryModel> addCategory(String businessId, FormData requestData);
+
+  Future<List<CategoryModel>> fetchAllCategory(String businessId);
+
+//Future<Business> fetchBusiness(String id);
+}
+
+class CategoryRepository extends ICategoryRepository {
+  final ICategoryDatasource categoryDatasource;
+
+  CategoryRepository(this.categoryDatasource);
+
+  @override
+  Future<CategoryModel> addCategory(String businessId, FormData requestData) {
+    return categoryDatasource.addCategory(businessId, requestData);
+  }
+
+  @override
+  Future<List<CategoryModel>> fetchAllCategory(String businessId) {
+    return categoryDatasource.fetchAllCategory(businessId);
+  }
+
+// @override
+// Future<Business> fetchBusiness(String id) {
+//   return businessDatasource.fetchBusiness(id);
+// }
+}
