83f549feff91ee1656dc61efa529d9ed3f4900b1
habyaad <adeomotoshan@gmail.com>
2025-03-02 13:47:49 +0100

    feat: implement more business methods

--- lib/datasource/data/business_datasource.dart
+++ lib/datasource/data/business_datasource.dart
@@ -2,14 +2,15 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:payvidence/model/business_model.dart';
-import 'package:payvidence/shared_dependency/shared_dependency.dart';
-
 import '../../data/network/api_response.dart';
 import '../../data/network/network_service.dart';
 import '../../utilities/payvidence_endpoints.dart';
 
 abstract class IBusinessDatasource {
   Future<Business> addBusiness(FormData requestData);
+  Future<List<Business>> fetchAllBusiness();
+  Future<Business> fetchBusiness(String id);
+
 }
 
 class BusinessDatasource extends IBusinessDatasource {
@@ -22,9 +23,61 @@ class BusinessDatasource extends IBusinessDatasource {
     //final NetworkService _networkService = locator<NetworkService>();
     try {
       final Either<Failure, Success> response = await networkService.post(
-          PayvidenceEndpoints.createBusiness,
+          PayvidenceEndpoints.business,
           data: requestData,
           headers: {"Content-Type": "multipart/form-data"});
+      //LoggerService.info("Product Categories:: ${response.toString()}");
+
+      return response.fold((fail) {
+        throw fail.error;
+      }, (success) {
+        return Business.fromJson(success.data);
+      });
+    } catch (e) {
+      if (kDebugMode) {
+        print("Error $e");
+      }
+
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<Business>> fetchAllBusiness() async {
+    try {
+      final Either<Failure, Success> response = await networkService.get(
+          PayvidenceEndpoints.business,
+          //data: requestData,
+          //headers: {"Content-Type": "multipart/form-data"}
+      );
+
+      //LoggerService.info("Product Categories:: ${response.toString()}");
+
+      return response.fold((fail) {
+        throw fail.error;
+      }, (success) {
+        List jsonList = success.data['data'] as List;
+       // print(success.data);
+        return jsonList.map((json) => Business.fromJson(json)).toList();
+      });
+    } catch (e) {
+      if (kDebugMode) {
+
+        print("Error $e");
+      }
+
+      rethrow;
+    }
+  }
+
+  @override
+  Future<Business> fetchBusiness(String id) async {
+    try {
+      final Either<Failure, Success> response = await networkService.get(
+        '${PayvidenceEndpoints.business}$id',
+        //data: requestData,
+        //headers: {"Content-Type": "multipart/form-data"}
+      );
       //LoggerService.info("Product Categories:: ${response.toString()}");
 
       return response.fold((fail) {
--- lib/repositories/repository/business_repository.dart
+++ lib/repositories/repository/business_repository.dart
@@ -3,13 +3,17 @@ import 'package:payvidence/datasource/data/business_datasource.dart';
 
 import '../../model/business_model.dart';
 
-abstract class IBusinessRepository{
+abstract class IBusinessRepository {
   Future<Business> addBusiness(FormData requestData);
 
+  Future<List<Business>> fetchAllBusiness();
+
+  Future<Business> fetchBusiness(String id);
 }
 
-class BusinessRepository extends IBusinessRepository{
+class BusinessRepository extends IBusinessRepository {
   final IBusinessDatasource businessDatasource;
+
   BusinessRepository(this.businessDatasource);
 
   @override
@@ -17,4 +21,13 @@ class BusinessRepository extends IBusinessRepository{
     return businessDatasource.addBusiness(requestData);
   }
 
+  @override
+  Future<List<Business>> fetchAllBusiness() {
+    return businessDatasource.fetchAllBusiness();
+  }
+
+  @override
+  Future<Business> fetchBusiness(String id) {
+    return businessDatasource.fetchBusiness(id);
+  }
 }
