78bee87bca21a4eddb09a424248d286772d47578
habyaad <adeomotoshan@gmail.com>
2025-03-02 13:42:23 +0100

    feat: create category model

---
+++ lib/model/category_model.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:equatable/equatable.dart';
+
+class CategoryModel extends Equatable{
+  final String? id;
+  final String? businessId;
+  final String? name;
+  final String? description;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+
+  const CategoryModel({
+    this.id,
+    this.businessId,
+    this.name,
+    this.description,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  factory CategoryModel.fromRawJson(String str) => CategoryModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory CategoryModel.fromJson(Map<String, dynamic> json) => CategoryModel(
+    id: json["id"],
+    businessId: json["business_id"],
+    name: json["name"],
+    description: json["description"],
+    createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
+    updatedAt: json["updated_at"] == null ? null : DateTime.parse(json["updated_at"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "business_id": businessId,
+    "name": name,
+    "description": description,
+    "created_at": createdAt?.toIso8601String(),
+    "updated_at": updatedAt?.toIso8601String(),
+  };
+
+  @override
+  List<Object?> get props => [id,
+    businessId,
+    name,
+    description,
+    createdAt,
+    updatedAt,];
+}
