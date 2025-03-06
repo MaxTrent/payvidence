bc1965fde5809adea87f7c0517288a54dbf82a42
habyaad <adeomotoshan@gmail.com>
2025-03-06 12:36:07 +0100

    feat: create get all category provider

---
+++ lib/providers/category_providers/get_all_category_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:payvidence/model/category_model.dart';
+import 'package:payvidence/repositories/repository/business_repository.dart';
+
+import '../../model/business_model.dart';
+import '../../repositories/repository/category_repository.dart';
+import '../../shared_dependency/shared_dependency.dart';
+import '../business_providers/current_business_provider.dart';
+
+final getAllCategoryProvider =
+    AsyncNotifierProvider<GetAllCategoryNotifier, List<CategoryModel>>(() {
+  return GetAllCategoryNotifier();
+});
+
+class GetAllCategoryNotifier extends AsyncNotifier<List<CategoryModel>> {
+  @override
+  Future<List<CategoryModel>> build() {
+    //final userModel = getUser();
+    return locator<ICategoryRepository>()
+        .fetchAllCategory(ref.watch(getCurrentBusinessProvider)!.id!);
+  }
+
+  Future<CategoryModel> addCategory(Map<String, dynamic> data) {
+    return locator<ICategoryRepository>()
+        .addCategory(ref.watch(getCurrentBusinessProvider)!.id!, data);
+  }
+
+// Add methods to mutate the state
+}
