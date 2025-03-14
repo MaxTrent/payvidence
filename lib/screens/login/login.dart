0060b4df96044659638ef2f11cda961522ce8912
habyaad <adeomotoshan@gmail.com>
2025-03-14 10:59:29 +0100

    feat: reject back button pop on login

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -34,12 +34,8 @@ class Login extends HookConsumerWidget {
     final obscureText = useState(true);
 
     bool areFieldsEmpty() {
-      return emailController.text
-          .toString()
-          .isEmpty ||
-          passwordController.text
-              .toString()
-              .isEmpty;
+      return emailController.text.toString().isEmpty ||
+          passwordController.text.toString().isEmpty;
     }
 
     useEffect(() {
@@ -62,148 +58,136 @@ class Login extends HookConsumerWidget {
 
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus!.unfocus,
-      child: Scaffold(
-        body: Form(
-          key: _formKey,
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w),
-            child: SafeArea(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 16.h,
-                  ),
-                  Text(
-                    'Log in',
-                    style: Theme
-                        .of(context)
-                        .textTheme
-                        .displayLarge,
-                  ),
-                  SizedBox(
-                    height: 8.h,
-                  ),
-                  Text(
-                    'Log in with your email address and password',
-                    style: Theme
-                        .of(context)
-                        .textTheme
-                        .displaySmall!,
-                  ),
-                  SizedBox(
-                    height: 32.h,
-                  ),
-                  Text(
-                    'Email address',
-                    style: Theme
-                        .of(context)
-                        .textTheme
-                        .displaySmall,
-                  ),
-                  SizedBox(
-                    height: 8.h,
-                  ),
-                  AppTextField(
-                    hintText: 'Email address',
-                    controller:
-                    emailController,
-                    validator: (val) {
-                      if (!val!.isValidEmail || val.isEmpty) {
-                        return 'Enter valid email address';
-                      }
-                      return null;
-                    },
-                  ),
+      child: PopScope(
+        canPop: false,
+        child: Scaffold(
+          body: Form(
+            key: _formKey,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.w),
+              child: SafeArea(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 16.h,
+                    ),
+                    Text(
+                      'Log in',
+                      style: Theme.of(context).textTheme.displayLarge,
+                    ),
+                    SizedBox(
+                      height: 8.h,
+                    ),
+                    Text(
+                      'Log in with your email address and password',
+                      style: Theme.of(context).textTheme.displaySmall!,
+                    ),
+                    SizedBox(
+                      height: 32.h,
+                    ),
+                    Text(
+                      'Email address',
+                      style: Theme.of(context).textTheme.displaySmall,
+                    ),
+                    SizedBox(
+                      height: 8.h,
+                    ),
+                    AppTextField(
+                      hintText: 'Email address',
+                      controller: emailController,
+                      validator: (val) {
+                        if (!val!.isValidEmail || val.isEmpty) {
+                          return 'Enter valid email address';
+                        }
+                        return null;
+                      },
+                    ),
 
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  Text(
-                    'Password',
-                    style: Theme
-                        .of(context)
-                        .textTheme
-                        .displaySmall,
-                  ),
-                  SizedBox(
-                    height: 8.h,
-                  ),
-                  AppTextField(
-                    hintText: 'Password',
-                    controller: passwordController,
-                    validator: (val) {
-                      if (!val!.isValidPassword || val.isEmpty) {
-                        return 'Enter a valid password';
-                      }
-                      return null;
-                    },
-                    suffixIcon: Padding(
-                      padding: EdgeInsets.all(16.h),
-                      child: GestureDetector(
-                        onTap: () => obscureText.value = !obscureText.value,
-                        child: SvgPicture.asset(
-                          Assets.svg.password,
-                          height: 24.h,
-                          width: 24.w,
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Text(
+                      'Password',
+                      style: Theme.of(context).textTheme.displaySmall,
+                    ),
+                    SizedBox(
+                      height: 8.h,
+                    ),
+                    AppTextField(
+                      hintText: 'Password',
+                      controller: passwordController,
+                      validator: (val) {
+                        if (!val!.isValidPassword || val.isEmpty) {
+                          return 'Enter a valid password';
+                        }
+                        return null;
+                      },
+                      suffixIcon: Padding(
+                        padding: EdgeInsets.all(16.h),
+                        child: GestureDetector(
+                          onTap: () => obscureText.value = !obscureText.value,
+                          child: SvgPicture.asset(
+                            Assets.svg.password,
+                            height: 24.h,
+                            width: 24.w,
+                          ),
                         ),
                       ),
+                      obscureText: obscureText.value,
                     ),
-                    obscureText: obscureText.value,
-                  ),
-                  SizedBox(
-                    height: 20.h,
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      locator<PayvidenceAppRouter>().navigateNamed(
-                          PayvidenceRoutes.forgotPassword);
-                      // context.push('/forgotPassword');
-                    },
-                    child: Text(
-                      'Forgot password?',
-                      style: Theme
-                          .of(context)
-                          .textTheme
-                          .displayMedium!
-                          .copyWith(fontSize: 14.sp, color: primaryColor2),
+                    SizedBox(
+                      height: 20.h,
                     ),
-                  ),
-                  SizedBox(
-                    height: 32.h,
-                  ),
-                  // viewModel.loginState.isLoading ? const LoadingIndicator():
-                  AppButton(
-                    buttonText: 'Log in',
+                    GestureDetector(
+                      onTap: () {
+                        locator<PayvidenceAppRouter>()
+                            .navigateNamed(PayvidenceRoutes.forgotPassword);
+                        // context.push('/forgotPassword');
+                      },
+                      child: Text(
+                        'Forgot password?',
+                        style: Theme.of(context)
+                            .textTheme
+                            .displayMedium!
+                            .copyWith(fontSize: 14.sp, color: primaryColor2),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 32.h,
+                    ),
+                    // viewModel.loginState.isLoading ? const LoadingIndicator():
+                    AppButton(
+                      buttonText: 'Log in',
+                      isDisabled: _areFieldsEmpty.value,
+                      isProcessing: viewModel.isLoading,
+                      backgroundColor: !_areFieldsEmpty.value
+                          ? primaryColor2
+                          : primaryColor2.withOpacity(0.4),
+                      onPressed: () {
+                        print("Button pressed");
+                        print("Fields empty: ${_areFieldsEmpty.value}");
 
-                    isDisabled: _areFieldsEmpty.value,
-                    isProcessing: viewModel.isLoading,
-                    backgroundColor: !_areFieldsEmpty.value
-                        ? primaryColor2
-                        : primaryColor2.withOpacity(0.4),
-                    onPressed: () {
-                      print("Button pressed");
-                      print("Fields empty: ${_areFieldsEmpty.value}");
-
-                      if (_formKey.currentState!.validate()) {
-                        log("Form is valid");
-                        FocusScope.of(context).unfocus();
-                        viewModel.login(
-                          email: emailController.text.trim(),
-                          password: passwordController.text.trim(),
-                          navigateOnSuccess: () {
-                            locator<PayvidenceAppRouter>().popUntil(
-                                    (route) => route is OnboardingScreen);
-                            locator<PayvidenceAppRouter>().replaceNamed(
-                                PayvidenceRoutes.home);
-                          },
-                        );
-                      } else {
-                        log("Form is not valid");
-                      }
-                    },
-                  ),
-                ],
+                        if (_formKey.currentState!.validate()) {
+                          log("Form is valid");
+                          FocusScope.of(context).unfocus();
+                          viewModel.login(
+                            email: emailController.text.trim(),
+                            password: passwordController.text.trim(),
+                            navigateOnSuccess: () {
+                              locator<PayvidenceAppRouter>()
+                                  .popUntil((route) => route is OnboardingScreen);
+                              locator<PayvidenceAppRouter>()
+                                  .replaceNamed(PayvidenceRoutes.home);
+                            },
+                          );
+                        } else {
+                          log("Form is not valid");
+                        }
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
