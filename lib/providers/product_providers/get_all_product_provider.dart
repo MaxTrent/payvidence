e81cd78fa0d723f26b1510b85545a6f85966e37b
habyaad <adeomotoshan@gmail.com>
2025-03-14 06:41:11 +0100

    feat: expose other methods in get all product provider

--- lib/providers/product_providers/get_all_product_provider.dart
+++ lib/providers/product_providers/get_all_product_provider.dart
@@ -1,6 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:payvidence/model/product_model.dart';
 import 'package:payvidence/providers/business_providers/current_business_provider.dart';
+import 'package:payvidence/providers/product_providers/product_fillter_provider.dart';
 import 'package:payvidence/repositories/repository/business_repository.dart';
 import 'package:payvidence/repositories/repository/product_repository.dart';
 
@@ -20,5 +22,44 @@ class GetAllProductNotifier extends AsyncNotifier<List<Product>> {
         .fetchAllProduct(ref.watch(getCurrentBusinessProvider)!.id!);
   }
 
+  Future<void> deleteProduct(
+    String productId,
+  ) async {
+    try {
+      await locator<IProductRepository>().deleteProduct(productId);
+      await setFilter();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Product> updateProduct(
+    FormData requestData,
+    String productId,
+  ) async {
+    try {
+      return await locator<IProductRepository>()
+          .updateProduct(requestData, productId);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Product> restockProduct(String productId, int quantity) async {
+    try {
+      return await locator<IProductRepository>()
+          .restockProduct(productId, quantity);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> setFilter() async {
+    final Map<String, dynamic> filterData = ref.read(productFilterProvider);
+    print(filterData);
+    state = AsyncData(await locator<IProductRepository>().fetchAllProduct(
+        ref.watch(getCurrentBusinessProvider)!.id!,
+        categoryId: filterData['category_id'] ?? ''));
+  }
 // Add methods to mutate the state
 }
