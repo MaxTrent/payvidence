ffbdfad47e98d8f3ee49edda46723bd42d3f7ea4
habyaad <adeomotoshan@gmail.com>
2025-03-14 06:38:33 +0100

    refactor: update model

--- lib/model/product_model.dart
+++ lib/model/product_model.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
 
-import 'package:equatable/equatable.dart';
+import 'brand_model.dart';
+import 'category_model.dart';
 
-class Product extends Equatable {
+class Product {
   final String? id;
   final String? name;
   final String? businessId;
@@ -16,8 +17,10 @@ class Product extends Equatable {
   final String? vat;
   final DateTime? createdAt;
   final DateTime? updatedAt;
+  final BrandModel? brand;
+  final CategoryModel? category;
 
-  const Product({
+  Product({
     this.id,
     this.name,
     this.businessId,
@@ -31,6 +34,8 @@ class Product extends Equatable {
     this.vat,
     this.createdAt,
     this.updatedAt,
+    this.brand,
+    this.category,
   });
 
   factory Product.fromRawJson(String str) => Product.fromJson(json.decode(str));
@@ -38,55 +43,39 @@ class Product extends Equatable {
   String toRawJson() => json.encode(toJson());
 
   factory Product.fromJson(Map<String, dynamic> json) => Product(
-        id: json["id"],
-        name: json["name"],
-        businessId: json["business_id"],
-        categoryId: json["category_id"],
-        brandId: json["brand_id"],
-        description: json["description"],
-        logoUrl: json["logo_url"],
-        price: json["price"],
-        quantitySold: json["quantity_sold"],
-        quantityAvailable: json["quantity_available"],
-        vat: json["vat"],
-        createdAt: json["created_at"] == null
-            ? null
-            : DateTime.parse(json["created_at"]),
-        updatedAt: json["updated_at"] == null
-            ? null
-            : DateTime.parse(json["updated_at"]),
-      );
+    id: json["id"],
+    name: json["name"],
+    businessId: json["business_id"],
+    categoryId: json["category_id"],
+    brandId: json["brand_id"],
+    description: json["description"],
+    logoUrl: json["logo_url"],
+    price: json["price"],
+    quantitySold: json["quantity_sold"],
+    quantityAvailable: json["quantity_available"],
+    vat: json["vat"],
+    createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
+    updatedAt: json["updated_at"] == null ? null : DateTime.parse(json["updated_at"]),
+    brand: json["brand"] == null ? null : BrandModel.fromJson(json["brand"]),
+    category: json["category"] == null ? null : CategoryModel.fromJson(json["category"]),
+  );
 
   Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-        "business_id": businessId,
-        "category_id": categoryId,
-        "brand_id": brandId,
-        "description": description,
-        "logo_url": logoUrl,
-        "price": price,
-        "quantity_sold": quantitySold,
-        "quantity_available": quantityAvailable,
-        "vat": vat,
-        "created_at": createdAt?.toIso8601String(),
-        "updated_at": updatedAt?.toIso8601String(),
-      };
-
-  @override
-  List<Object?> get props => [
-        id,
-        name,
-        businessId,
-        categoryId,
-        brandId,
-        description,
-        logoUrl,
-        price,
-        quantitySold,
-        quantityAvailable,
-        vat,
-        createdAt,
-        updatedAt,
-      ];
+    "id": id,
+    "name": name,
+    "business_id": businessId,
+    "category_id": categoryId,
+    "brand_id": brandId,
+    "description": description,
+    "logo_url": logoUrl,
+    "price": price,
+    "quantity_sold": quantitySold,
+    "quantity_available": quantityAvailable,
+    "vat": vat,
+    "created_at": createdAt?.toIso8601String(),
+    "updated_at": updatedAt?.toIso8601String(),
+    "brand": brand?.toJson(),
+    "category": category?.toJson(),
+  };
 }
+
