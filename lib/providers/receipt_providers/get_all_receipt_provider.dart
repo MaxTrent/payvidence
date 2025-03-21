3ed7e3293e896271d4da45de97f235447b86c7c3
habyaad <adeomotoshan@gmail.com>
2025-03-21 09:48:20 +0100

    feat: create get all receipt provider

---
+++ lib/providers/receipt_providers/get_all_receipt_provider.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:payvidence/model/product_model.dart';
+import 'package:payvidence/providers/business_providers/current_business_provider.dart';
+import 'package:payvidence/providers/product_providers/product_fillter_provider.dart';
+import 'package:payvidence/repositories/repository/product_repository.dart';
+import 'package:payvidence/repositories/repository/receipt_repository.dart';
+import '../../model/receipt_model.dart';
+import '../../shared_dependency/shared_dependency.dart';
+
+final getAllReceiptProvider =
+    AsyncNotifierProvider<GetAllReceiptNotifier, List<Receipt>>(() {
+  return GetAllReceiptNotifier();
+});
+
+class GetAllReceiptNotifier extends AsyncNotifier<List<Receipt>> {
+  @override
+  Future<List<Receipt>> build() {
+    //final userModel = getUser();
+    return locator<IReceiptRepository>().fetchAllReceipts(
+        ref.watch(getCurrentBusinessProvider)!.id!, 'receipt');
+  }
+
+  Future<Receipt> addReceipt(Map<String, dynamic> data) {
+    return locator<IReceiptRepository>().createReceipt(data);
+  }
+// Add methods to mutate the state
+}
