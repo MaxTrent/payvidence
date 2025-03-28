8e14184ffe80be41c8a583b12ac881501cd6ffc2
habyaad <adeomotoshan@gmail.com>
2025-03-28 10:11:03 +0100

    feat: extend a method on the string type

--- lib/utilities/extensions.dart
+++ lib/utilities/extensions.dart
@@ -1,18 +1,10 @@
+import 'package:intl/intl.dart';
+
 extension NumberFormatting on String {
-  String toCommaSeparated() {
-    final numberStr = toString();
-    final buffer = StringBuffer();
-    int count = 0;
-
-    for (int i = numberStr.length - 1; i >= 0; i--) {
-      buffer.write(numberStr[i]);
-      count++;
-      if (count % 3 == 0 && i != 0) {
-        buffer.write(',');
-      }
-    }
-
-    return buffer.toString().split('').reversed.join('');
+  String commaSeparated() {
+    final number = double.tryParse(this) ?? 0.0;
+    final format = NumberFormat("#,##0.##", "en_US");
+    return format.format(number);
   }
 
   String capitalize() {
