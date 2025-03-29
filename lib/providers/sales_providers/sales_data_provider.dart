9409fd6944f60c4f32d1df6b84de78a5d33ceb71
habyaad <adeomotoshan@gmail.com>
2025-03-29 13:01:42 +0100

    feat: implement sales

--- lib/providers/sales_providers/sales_data_provider.dart
+++ lib/providers/sales_providers/sales_data_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:payvidence/providers/sales_providers/sales_fillter_provider.dart';
 
 import '../../model/sales_model.dart';
 import '../../repositories/repository/sales_repository.dart';
@@ -6,15 +7,24 @@ import '../../shared_dependency/shared_dependency.dart';
 import '../business_providers/current_business_provider.dart';
 
 final salesDataProvider =
-    AsyncNotifierProvider<GetSalesDataNotifier, List<Sales>>(() {
+    AsyncNotifierProvider<GetSalesDataNotifier, Sales>(() {
   return GetSalesDataNotifier();
 });
 
-class GetSalesDataNotifier extends AsyncNotifier<List<Sales>> {
+class GetSalesDataNotifier extends AsyncNotifier<Sales> {
   @override
-  Future<List<Sales>> build() {
+  Future<Sales> build() {
     //final userModel = getUser();
     return locator<ISalesRepository>()
         .fetchSales(ref.watch(getCurrentBusinessProvider)!.id!);
+  }
+
+  Future<void> setFilter() async {
+    final Map<String, dynamic> filterData = ref.read(salesFilterProvider);
+    print(filterData);
+    state = AsyncData(await locator<ISalesRepository>().fetchSales(
+        ref.watch(getCurrentBusinessProvider)!.id!,
+        interval: filterData['interval'] ?? ''));
+    //name: filterData['name'] ?? ''));
   }
 }
--- lib/screens/sales/sales.dart
+++ lib/screens/sales/sales.dart
@@ -1,21 +1,28 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:payvidence/components/app_text_field.dart';
 import 'package:payvidence/constants/app_colors.dart';
+import 'package:payvidence/providers/sales_providers/sales_data_provider.dart';
+import 'package:payvidence/providers/sales_providers/sales_fillter_provider.dart';
+import 'package:payvidence/screens/all_receipts/receipt_screen.dart';
+import 'package:payvidence/utilities/extensions.dart';
 
+import '../../components/custom_shimmer.dart';
 import '../../gen/assets.gen.dart';
 
-
 @RoutePage(name: 'SalesRoute')
-class Sales extends StatelessWidget {
+class Sales extends ConsumerWidget {
   Sales({super.key});
 
   final _controller = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final salesData = ref.watch(salesDataProvider);
+    final interval = ref.watch(salesFilterProvider)["interval"];
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -25,95 +32,162 @@ class Sales extends StatelessWidget {
               12.verticalSpace,
               Row(
                 children: [
-                  Container(
-                    height: 45.h,
-                    width: 83.w,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(43.r),
-                        color: primaryColor2),
-                    child: Center(
-                        child: Text(
-                      'Weekly',
-                      style: Theme.of(context)
-                          .textTheme
-                          .displaySmall!
-                          .copyWith(color: Colors.white),
-                    )),
+                  GestureDetector(
+                    onTap: () {
+                      if(interval == "weekly"){
+                        return;
+                      }
+                      ref
+                          .read(salesFilterProvider.notifier)
+                          .setKey("interval", "weekly");
+                      ref.read(salesDataProvider.notifier).setFilter();
+                    },
+                    child: Container(
+                      height: 45.h,
+                      width: 83.w,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(43.r),
+                          color: interval == "weekly"
+                              ? primaryColor2
+                              : Colors.transparent),
+                      child: Center(
+                          child: Text(
+                        'Weekly',
+                        style: Theme.of(context)
+                            .textTheme
+                            .displaySmall!
+                            .copyWith(
+                                color: interval == "weekly"
+                                    ? Colors.white
+                                    : Colors.black),
+                      )),
+                    ),
                   ),
                   SizedBox(width: 12.w),
-                  Container(
-                    height: 45.h,
-                    width: 83.w,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(43.r),
-                      // color: primaryColor2
+                  GestureDetector(
+                    onTap: () {
+                      if(interval == "monthly"){
+                        return;
+                      }
+                      ref
+                          .read(salesFilterProvider.notifier)
+                          .setKey("interval", "monthly");
+                      ref.read(salesDataProvider.notifier).setFilter();
+
+                    },
+                    child: Container(
+                      height: 45.h,
+                      width: 83.w,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(43.r),
+                          color: interval == "monthly"
+                              ? primaryColor2
+                              : Colors.transparent),
+                      child: Center(
+                          child: Text(
+                        'Monthly',
+                        style: Theme.of(context)
+                            .textTheme
+                            .displaySmall!
+                            .copyWith(
+                                color: interval == "monthly"
+                                    ? Colors.white
+                                    : Colors.black),
+                      )),
                     ),
-                    child: Center(
-                        child: Text(
-                      'Monthly',
-                      style: Theme.of(context).textTheme.displaySmall!,
-                    )),
                   ),
                   SizedBox(width: 12.w),
-                  Container(
-                    height: 45.h,
-                    width: 83.w,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(43.r),
-                      // color: primaryColor2
+                  GestureDetector(
+                    onTap: () {
+                      if(interval == "yearly"){
+                        return;
+                      }
+                      ref
+                          .read(salesFilterProvider.notifier)
+                          .setKey("interval", "yearly");
+                      ref.read(salesDataProvider.notifier).setFilter();
+
+                    },
+                    child: Container(
+                      height: 45.h,
+                      width: 83.w,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(43.r),
+                          color: interval == "yearly"
+                              ? primaryColor2
+                              : Colors.transparent),
+                      child: Center(
+                          child: Text(
+                        'Yearly',
+                        style: Theme.of(context)
+                            .textTheme
+                            .displaySmall!
+                            .copyWith(
+                                color: interval == "yearly"
+                                    ? Colors.white
+                                    : Colors.black),
+                      )),
                     ),
-                    child: Center(
-                        child: Text(
-                      'Yearly',
-                      style: Theme.of(context).textTheme.displaySmall!,
-                    )),
                   ),
                 ],
               ),
-              SizedBox(
-                height: 24.h,
-              ),
-              AppTextField(
-                hintText: '08 / 09 / 2024',
-                controller: _controller,
-                suffixIcon: const Icon(Icons.keyboard_arrow_down),
-              ),
+              // SizedBox(
+              //   height: 24.h,
+              // ),
+              // AppTextField(
+              //   hintText: '08 / 09 / 2024',
+              //   controller: _controller,
+              //   suffixIcon: const Icon(Icons.keyboard_arrow_down),
+              // ),
               SizedBox(
                 height: 36.h,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SalesInfoTile(
-                    icon: Assets.svg.statusUp,
-                    amount: '₦800K',
-                    description: 'Total revenue',
-                  ),
-                  SalesInfoTile(
-                    icon: Assets.svg.boxTick,
-                    amount: '320',
-                    description: 'Total sales',
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 18.h,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SalesInfoTile(
-                    icon: Assets.svg.noteText,
-                    amount: '320',
-                    description: 'Total receipts',
-                  ),
-                  SalesInfoTile(
-                    icon: Assets.svg.archiveBook,
-                    amount: '102',
-                    description: 'Total invoices',
-                  ),
-                ],
-              ),
+              salesData.when(data: (data) {
+                return Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SalesInfoTile(
+                          icon: Assets.svg.statusUp,
+                          amount:
+                              '₦${data.totalRevenue.toString().commaSeparated()}',
+                          description: 'Total revenue',
+                        ),
+                        SalesInfoTile(
+                          icon: Assets.svg.boxTick,
+                          amount: data.totalSales.toString().commaSeparated(),
+                          description: 'Total sales',
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 18.h,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SalesInfoTile(
+                          icon: Assets.svg.noteText,
+                          amount:
+                              data.totalReceipts.toString().commaSeparated(),
+                          description: 'Total receipts',
+                        ),
+                        SalesInfoTile(
+                          icon: Assets.svg.archiveBook,
+                          amount:
+                              data.totalInvoices.toString().commaSeparated(),
+                          description: 'Total invoices',
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+              }, error: (error, _) {
+                return const Text('An error has occurred');
+              }, loading: () {
+                return const CustomShimmer();
+              }),
               SizedBox(
                 height: 36.h,
               ),
@@ -149,7 +223,7 @@ class SalesInfoTile extends StatelessWidget {
       ),
       child: Padding(
         padding:
-            EdgeInsets.only(left: 16.w, right: 16.w, top: 16.h, bottom: 14.h),
+            EdgeInsets.only(left: 12.w, right: 12.w, top: 16.h, bottom: 14.h),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -170,12 +244,15 @@ class SalesInfoTile extends StatelessWidget {
                 SizedBox(
                   width: 8.w,
                 ),
-                Text(
-                  amount,
-                  style: Theme.of(context)
-                      .textTheme
-                      .displayLarge!
-                      .copyWith(fontSize: 22.sp),
+                Expanded(
+                  child: Text(
+                    amount,
+                    style: Theme.of(context)
+                        .textTheme
+                        .displayLarge!
+                        .copyWith(fontSize: 14.sp),
+                    overflow: TextOverflow.ellipsis,
+                  ),
                 )
               ],
             ),
