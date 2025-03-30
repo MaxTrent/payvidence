4ef12a36029aa628802c18e764fff848aeaa47a4
habyaad <adeomotoshan@gmail.com>
2025-03-30 21:11:11 +0100

    feat: implement sales chart

--- lib/datasource/data/sales_datasource.dart
+++ lib/datasource/data/sales_datasource.dart
@@ -21,7 +21,7 @@ class SalesDatasource extends ISalesDatasource {
       {String? startDate, String? endDate, String? interval}) async {
     startDate ??= "2024-01-03";
     endDate ??= DateFormat("y-M-d").format(DateTime.now());
-    interval ??= "monthly";
+    interval ??= "weekly";
     try {
       final Either<Failure, Success> response = await networkService.get(
         '${PayvidenceEndpoints.analytics}?business_id=$businessId&start_date=$startDate&end_date=$endDate&interval=$interval',
