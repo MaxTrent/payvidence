1efb2dbe668a1e2a55aaa074c60a32c4b3e4bfcf
Samuel <samueladelowo92@gmail.com>
2025-06-28 23:36:39 +0100

    fix screen nav logic when deleting a brand and category

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:payvidence/data/local/session_constants.dart';
 import 'package:payvidence/routes/payvidence_app_router.gr.dart';
 import 'package:payvidence/shared_dependency/shared_dependency.dart';
+import 'package:payvidence/gen/assets.gen.dart';
 
 import '../data/local/session_manager.dart';
 
@@ -15,29 +16,57 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
+
   @override
   void initState() {
     super.initState();
+    
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1500),
+      vsync: this,
+    );
+    
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    ));
+    
+    _scaleAnimation = Tween<double>(
+      begin: 0.5,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.elasticOut,
+    ));
+    
+    _controller.forward();
     _checkAuthState();
   }
 
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   Future<void> _checkAuthState() async {
+    await Future.delayed(const Duration(milliseconds: 2000));
+    
     final sessionManager = locator<SessionManager>();
     final isLoggedIn = sessionManager.get<bool>(SessionConstants.isUserLoggedIn) ?? false;
-    // final isOnboarded = sessionManager.get<bool>(SessionConstants.isOnboarded) ?? false;
-
-    // developer.log('Splash: isLoggedIn=$isLoggedIn,  isOnboarded=$isOnboarded' );
 
     final router = AutoRouter.of(context);
     if (isLoggedIn) {
       router.replace(const HomePageRoute());
-    }
-    // else if (!isOnboarded) {
-    //   router.replace(OnboardingScreenRoute());
-    // }
-    else {
-      router.replace( OnboardingScreenRoute());
+    } else {
+      router.replace(OnboardingScreenRoute());
     }
   }
 
@@ -45,8 +74,23 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      body: const Center(
-        child: CircularProgressIndicator(),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return FadeTransition(
+              opacity: _fadeAnimation,
+              child: ScaleTransition(
+                scale: _scaleAnimation,
+                child: Image.asset(
+                  Assets.png.payvidenceLogo.path,
+                  width: 120,
+                  height: 120,
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
